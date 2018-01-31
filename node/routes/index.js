d91ac93e4bcb6a4ce62ff8a493b5b544835295f1
Sam <huangsaiy@hotmail.com>
2018-01-31 22:44:25 +1100

    add route

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+router.get('/reg', function(req, res, next) {
+  res.render('index', { title: 'Register' });
+});
+router.post('/reg', function(req, res, next) {
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+router.post('/login', function(req, res, next) {
+});
+router.get('/order', function(req, res, next) {
+  res.render('order', { title: 'Order' });
+});
+router.post('/order', function(req, res, next) {
+});
+router.get('/logout', function(req, res, next) {
+  res.render('logout', { title: 'Logout' });
 });
 
 module.exports = router;
