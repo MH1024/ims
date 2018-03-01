fab233e9bf25206d53bb9919d348d95812808993
Sam <huangsaiy@hotmail.com>
2018-03-01 23:28:11 +1100

    add register panel page

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegisterPanelComponent } from './register-panel/register-panel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPanelComponent
   ],
   imports: [
     BrowserModule
