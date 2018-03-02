859743f3ec7008b1966a31ecde8b6fb7b927f70b
Sam <huangsaiy@hotmail.com>
2018-03-02 22:20:46 +1100

    add auth components

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegisterPanelComponent } from './register-panel/register-panel.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterPanelComponent
+    RegisterPanelComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
