29db4a0089a4444f755232cfa800adba1e27217d
Sam <huangsaiy@hotmail.com>
2018-03-04 23:28:31 +1100

    add service for register page

--- angular-front/src/app/register-panel/register-panel.component.ts
+++ angular-front/src/app/register-panel/register-panel.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { authService, TokenPayload } from '../services/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-panel',
   templateUrl: './register-panel.component.html',
   styleUrls: ['./register-panel.component.css']
 })
-export class RegisterPanelComponent implements OnInit {
+export class RegisterPanelComponent {
+  credentials: TokenPayload = {
+    email: '',
+    name: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private auth: authService, private router: Router) {}
 
-  ngOnInit() {
+  register() {
+    this.auth.register(this.credentials).subscribe(() => {
+      this.router.navigateByUrl('/profile');
+    }, (err) => {
+      console.error(err);
+    });
   }
-
 }
--- angular-front/src/app/services/auth-service.service.ts
+++ angular-front/src/app/services/auth-service.service.ts
@@ -1,8 +1,68 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+import { Router } from '@angular/router';
+
+export interface UserDetails {
+  _id: string;
+  email: string;
+  name: string;
+  exp: number;
+  iat: number;
+}
+
+interface TokenResponse {
+  token: string;
+}
+
+export interface TokenPayload {
+  email: string;
+  password: string;
+  name?: string;
+}
 
 @Injectable()
-export class AuthServiceService {
+export class authService {
+  private token: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private saveToken(token: string): void {
+    localStorage.setItem('mean-token', token);
+    this.token = token;
+  }
+
+  private getToken(): string {
+    if (!this.token) {
+      this.token = localStorage.getItem('mean-token');
+    }
+    return this.token;
+  }
+
+  private request(method: 'post'|'get', type: 'login'|'register'|'profile', user?: TokenPayload): Observable<any> {
+    let base;
+
+    if (method === 'post') {
+      base = this.http.post(`/api/${type}`, user);
+    } else {
+      base = this.http.get(`/api/${type}`, { headers: { Authorization: `Bearer ${this.getToken()}` }});
+    }
+
+    const request = base.pipe(
+      map((data: TokenResponse) => {
+        if (data.token) {
+          this.saveToken(data.token);
+        }
+        return data;
+      })
+    );
+
+    return request;
+  }
+
+  public register(user: TokenPayload): Observable<any> {
+    return this.request('post', 'register', user);
+  }
 
 }
